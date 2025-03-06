1eb987b0891749a9e132d7cecd9a480331cbbbd8
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2025-03-06 14:29:45 -0300

    Inicio Tela de Login
    Criado o projeto no Flutter e iniciado a tela de login

---
+++ android/app/src/main/kotlin/com/example/hortifruti_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hortifruti_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hortifruti_app/src/autenticacao/sign_in_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SignInScreen(),
+    );
+  }
+}
+
