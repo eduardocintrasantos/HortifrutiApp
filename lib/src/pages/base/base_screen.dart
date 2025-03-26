ae4c023b8a379ad4f33b53bc67c8046c93393ff7
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2025-03-26 09:19:52 -0300

    Botão finalizar carrinho

--- lib/src/pages/base/base_screen.dart
+++ lib/src/pages/base/base_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hortifruti_app/src/pages/cart/cart_tab.dart';
 import 'package:hortifruti_app/src/pages/home/home_tab.dart';
 
 class BaseScreen extends StatefulWidget {
@@ -20,7 +21,7 @@ class _BaseScreenState extends State<BaseScreen> {
         controller: pageControler,
         children: [
           HomeTab(),
-          Container(color: Colors.yellowAccent),
+          CartTab(),
           Container(color: Colors.blue),
           Container(color: Colors.purple),
         ],
---
+++ lib/src/pages/cart/cart_tab.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:hortifruti_app/src/services/utils_services.dart';
+
+class CartTab extends StatelessWidget {
+  CartTab({super.key});
+
+  final UtilsServices utilsServices = UtilsServices();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Carrinho', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.green,
+      ),
+
+      body: Column(
+        children: [
+          //Lista de produtos
+          Expanded(child: Placeholder()),
+          //Espaçamento
+          SizedBox(height: 20,),
+          //Total/Finalizar Venda
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(30),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade300,
+                  blurRadius: 3,
+                  spreadRadius: 2,
+                )
+              ] 
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text('Total geral', style: TextStyle(
+                  fontSize: 12
+                ),),
+                Text(utilsServices.priceToCurrency(50.5), style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold
+                ),),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: (){}, 
+                    child: Text('Finalizar', 
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18)
+                      )
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
